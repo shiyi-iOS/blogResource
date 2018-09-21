aa498b5bb1cf3520f3c739f712141ba4949e1f59
lishibo-iOS <870037485@qq.com>
2018-09-21 17:33:21 +0800

    提交项目

---
+++ demo/FMDBDemo/FMDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBDemo
+//
+//  Created by zjjk on 2018/8/30.
+//  Copyright © 2018年 zjjk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
